0269831369fa4814db5068ca200f44c1ac934aad
dancan-sandys <dancan.oruko96@gmail.com>
2020-11-12 22:03:09 +0300

    add a single task api

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
-    path('all-tasks/', views.alltasks, name="all_tasks")
+    url('all-tasks/', views.all_tasks, name="all_tasks"),
+    url(r'single-task/(\d+)', views.single_task, name="single_task")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -9,14 +9,21 @@ from .serializers import taskSerializer
 # Create your views here.
 
 
+
+
 @api_view(['GET'])
-def alltasks(request):
+def all_tasks(request):
     tasks = task.objects.all()
     serializer = taskSerializer(tasks, many = True)
 
     return Response(serializer.data)
 
+@api_view(['GET'])
+def single_task(request, id):
+    tasks = task.objects.get(id = id)
+    serializer = taskSerializer(tasks, many = False)
 
+    return Response(serializer.data)
 
 
 
