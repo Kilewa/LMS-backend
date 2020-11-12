54d00c7a95f0d7796e5231b724fd6f680e9d78b5
kilewa <kilewageorge230@gmail.com>
2020-11-12 09:01:14 +0300

    created user profile model

--- lmsproject/settings.py
+++ lmsproject/settings.py
@@ -49,9 +49,9 @@ INSTALLED_APPS = [
 ]
 
 cloudinary.config( 
-  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'),
-  api_key = os.environ.get('CLOUDINARY_API_KEY'), 
-  api_secret = os.environ.get('CLOUDINARY_API_SECRET'), 
+    cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key = os.environ.get('CLOUDINARY_API_KEY'), 
+    api_secret = os.environ.get('CLOUDINARY_API_SECRET'), 
 )
 
 MIDDLEWARE = [
