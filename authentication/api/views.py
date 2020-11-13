3e5ffc37e185fee73429146aa63373a6522fe9b2
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-13 08:39:21 +0300

    update serializers && views

--- authentication/api/views.py
+++ authentication/api/views.py
@@ -5,29 +5,37 @@ from rest_framework.views import APIView
 from authentication.models import User
 from authentication.api.serializers import UserSerializer
 
-""" 
+
 class CreateUser(APIView):
       def post(self, request):
           user = request.data
           serializer = UserSerializer(data=user)
 
           if serializer.is_valid():
+
               serializer.save()
-              return Response(serializer.data, status=status.HTTP_201_CREATED)
+              response="successfully registered new user"
+              email =user.email
+              username =user.username
+              role=user.role
 
-          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) """
+              return Response(response,email,username,role, status=status.HTTP_201_CREATED)
+
+          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST',])
+""" @api_view(['POST',])
 def CreateUser(request):
     if request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         data = {}
+
         if serializer.is_valid():
-                authentication = serializer.save()
+                user = serializer.save()
                 data['response']="successfully registered new user"
-                data['email'] = authentication.email
-                data['username'] = authentication.username
+                data['email'] =user.email
+                data['username'] =user.username
+                data['role']=user.role
         else:
                 data = serializer.errors
-        return Response(data)
+        return Response(data) """
