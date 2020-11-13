be23ab452b2cb8ec1b2a55ce6c1c37b38e220f39
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-13 08:00:11 +0300

    Update admin.py
    removed useradmin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,17 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-from .forms import UserRegistrationForm
-from .models import Users
-
-class UsersAdmin(UserAdmin):
-    list_display = ('pk', 'email','username','date_joined', 'last_login', 'is_admin','is_staff')
-    search_fields = ('pk', 'email','username',)
-    readonly_fields=('pk', 'date_joined', 'last_login')
-    
-    filter_horizontal = ()
-    list_filter = ()
-    fieldsets = ()
-
-admin.site.register(Users, UsersAdmin)
+admin.site.register(User)
