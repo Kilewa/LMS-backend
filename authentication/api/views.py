d1a827444c98a258139b237365bdb9a2333b10cb
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-13 08:01:21 +0300

    Updated from Users to User

--- authentication/api/views.py
+++ authentication/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
-from authentication.models import Users
+from authentication.models import User
 from authentication.api.serializers import UserSerializer
 
 """ 
--- authentication/forms.py
+++ authentication/forms.py
@@ -2,11 +2,11 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 
-from authentication.models import Users
+from authentication.models import User
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Add a valid email address.')
 
     class Meta:
-        model = Users
+        model = User
         fields = ('email', 'username','password','password2',)
