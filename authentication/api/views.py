d7819bebf0522526ddd168ced24c61315041a6de
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-13 17:09:33 +0300

    merged conflicts

--- authentication/api/views.py
+++ authentication/api/views.py
@@ -7,35 +7,18 @@ from authentication.api.serializers import UserSerializer
 
 
 class CreateUser(APIView):
-      def post(self, request):
-          user = request.data
-          serializer = UserSerializer(data=user)
-
-          if serializer.is_valid():
-
-              user = serializer.save()
-              response="successfully registered new user"
-              email =user.email
-              username =user.username
-              role=user.role
-
-              return Response(response,email,username,role, status=status.HTTP_201_CREATED)
-
-          return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-""" @api_view(['POST',])
-def CreateUser(request):
-    if request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
-        data = {}
+    def post(self, request):
+        user = request.data
+        serializer = UserSerializer(data=user)
 
         if serializer.is_valid():
-                user = serializer.save()
-                data['response']="successfully registered new user"
-                data['email'] =user.email
-                data['username'] =user.username
-                data['role']=user.role
-        else:
-                data = serializer.errors
-        return Response(data) """
+
+            user = serializer.save()
+            response="successfully registered new user"
+            email =user.email
+            username =user.username
+            role=user.role
+
+            return Response(response,email,username,role, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- authentication/models.py
+++ authentication/models.py
@@ -6,11 +6,23 @@ from django.contrib.auth.models import (
 
 class Role(models.Model):
     '''
+    The Role entries are managed by the system,
+    automatically created via a Django data migration.
+    '''
+    EMPLOYEE = 1
+    DEPT_HEAD = 2
+    ADMIN = 3
+    ROLE_CHOICES = (
+        (EMPLOYEE, 'Employee'),
+        (DEPT_HEAD, 'Dept_head'),
+        (ADMIN, 'Admin'),
+    )
+
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
+
+    def __str__(self):
+        return self.get_role_display()
+
+
 class User(AbstractUser):
-    USER_ROLES = (
-        (1, 'DEPARTMENT HEAD'),
-        (2, 'EMPLOYEE')
-    )
-    role = models.CharField(
-        verbose_name='user role',max_length=2, choices=USER_ROLES
-    )
+    roles = models.ManyToManyField(Role)
