8fcc10db4e8857d022bc8a5f5fe576fdd4ee2d5b
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-16 00:04:13 +0300

    Update views.py
    added a CreateUser class view

--- authentication/api/views.py
+++ authentication/api/views.py
@@ -1,24 +1,23 @@
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from authentication.models import User
-from authentication.api.serializers import UserSerializer
+from authentication.api.serializers import RegisterSerializer
+from .permissions import IsDepartmentHead, IsEmployee
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
-class CreateUser(APIView):
+class CreateUser(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
     def post(self, request):
         user = request.data
-        serializer = UserSerializer(data=user)
+        serializer = self.serializer_class(data=user)
 
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            user_data = serializer.data
 
-            user = serializer.save()
-            response="successfully registered new user"
-            email =user.email
-            username =user.username
-            role=user.role
-
-            return Response(response,email,username,role, status=status.HTTP_201_CREATED)
+            return Response(user_data,status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
