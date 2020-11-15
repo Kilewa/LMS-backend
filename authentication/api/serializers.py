9c0cab340185eb4c1e56872ac789948e9a9b3c0f
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-16 00:03:54 +0300

    Update serializers.py
    fixed username email problem

--- authentication/api/serializers.py
+++ authentication/api/serializers.py
@@ -4,15 +4,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
     '''
     creating registration serializer
     '''
     password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
-
     class Meta:
         model = User
-        fields = ['email','username', 'password','password2','role']
+        fields = ['email','username', 'password','password2']
         extra_kwargs = {
             'password': {'write_only':True}
         }
@@ -20,15 +19,30 @@ class UserSerializer(serializers.ModelSerializer):
         user = User(
             email=self.validated_data['email'],
             username=self.validated_data['username'],
-            role=self.validated_data['role']
         )
         password=self.validated_data['password']
         password2=self.validated_data['password2']
 
         if password != password2:
             raise serializers.ValidationError({'password':'Passwords must match.'})
-        
         user.set_password(password)
-
         user.save()
         return user
+
+    """ def validate(self, attrs):
+        user = User (
+        email = attrs.get('email', ''),
+        username = attrs.get('username', '')
+        )
+        password=attrs.get('password', '')
+        password2=attrs.get('password2', '')
+
+        if password != password2:
+            raise serializers.ValidationError({'password':'Passwords must match.'})
+
+        user.set_password(password)
+        user.save
+        return attrs
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data) """
