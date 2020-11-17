ed3476aaa04abbf827b0b89fbcebc6f5bc03c619
dancan-sandys <dancan.oruko96@gmail.com>
2020-11-17 09:59:18 +0300

    Add function to create users and employees simultaneously

--- departments/api/serializers.py
+++ departments/api/serializers.py
@@ -13,11 +13,9 @@ class DepartmentSerializer(serializers.ModelSerializer):
 class EmployeeSerializer(serializers.ModelSerializer):
     """Employee JSON serializer"""
 
+    
+
     class Meta:
         model = Employee
         fields = '__all__'
 
-    def create(self, validated_data):
-        employee = Employee.objects.create(**validated_data)
-        employee.save()
-        return employee
--- departments/api/views.py
+++ departments/api/views.py
@@ -2,7 +2,15 @@ from rest_framework import generics, response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
-
+from authentication.models import User
+from authentication.api.utils import Util
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from authentication.api.serializers import RegisterSerializer, LoginSerializer
+import jwt
+from django.conf import settings
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from departments.models import Department, Employee
 from .serializers import DepartmentSerializer, EmployeeSerializer
@@ -27,8 +35,22 @@ class EmployeeCreateView(generics.ListCreateAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = EmployeeSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
+            user_data = serializer.data
+            user = User.objects.get(email=user_data['email'])
+
+            token=RefreshToken.for_user(user).access_token
+
+            current_site = get_current_site(request).domain
+
+            relativeLink = reverse('verifyaccount')
+
+            absurl='http://'+current_site+relativeLink+"?token="+str(token)
+            email_body = 'Hi '+user.username+' use link below to verify your email: \n'+ absurl
+            data={'email_body':email_body,'to_user':user.email, 'email_subject': 'Account Activation'}
+
+            Util.send_email(data)
             return response.Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
