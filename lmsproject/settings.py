48cbc5e629c456e045f228bcb6bdfd54ff39991e
kilewa <kilewageorge230@gmail.com>
2020-11-20 08:53:42 +0300

    heroku deployment

--- lmsproject/settings.py
+++ lmsproject/settings.py
@@ -156,19 +156,19 @@ SIMPLE_JWT = {
 
 WSGI_APPLICATION = 'lmsproject.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# # Database
+# # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'USER': config ('DB_USER'),
-        'HOST': config('DB_HOST','127.0.0.1'),
-        'PORT': config('DB_PORT', 5432),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'USER': config ('DB_USER'),
+#         'HOST': config('DB_HOST','127.0.0.1'),
+#         'PORT': config('DB_PORT', 5432),
+#     }
+# }
 
 
 # Password validation
